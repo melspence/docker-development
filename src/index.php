71dc55ae0795276e61f99fe102350e74c4ea50c0
Melville Spence <melvillespencer@hamnavoesolutions.com>
2023-02-20 14:29:26 +0000

    added PHP Service

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Hello there</title>
+    <style>
+        .center {
+            display: block;
+          margin-left: auto;
+          margin-right: auto;
+          width: 50%;
+        }
+</head>
+<body>
+    <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
+</body>
+</html>
